2226b3193ba9862ef08d10ec4f568bdb815d528e
arima0714 <a1710021@edu.cc.uec.ac.jp>
2019-07-09 14:29:11 +0900

    う〜ん謎

--- scheme(12~15)/task2.rkt
+++ scheme(12~15)/task2.rkt
@@ -2,3 +2,20 @@
   (read
    (open-input-file "./kakeizu")))
 
+"入力"
+kakeizu
+"入力"
+
+(define get-depth
+  (lambda(tree num)
+    (cond ((= num 1)
+              ()  
+          );1になった時 -> この時は出力される
+          (else
+              ()
+          );1でない時 -> この時は息子をたどるようにする
+    )
+  )
+)
+
+(get-depth kakeizu 3)
