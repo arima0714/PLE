f256d4b4a70cb0e208489d4e88f8fb5ce5d7442b
arima0714 <a1710021@edu.cc.uec.ac.jp>
2019-07-09 11:12:08 +0900

    ヒントの雛形を写した

--- scheme(12~15)/task1.rkt
+++ scheme(12~15)/task1.rkt
@@ -1,2 +1,16 @@
 (define TREE '(1 (2 (3 4)) 6 (7 8 9)) )
 
+(define mymap
+  (lambda (fn ls)
+    (if (null? ls)
+        '()
+        (cons (fn (car ls))
+              (map fn (cdr ls)))
+        )))
+
+(define map-tree
+  (lambda (fn tree)
+    (cond ((null? tree) '())
+          ((pair? tree) [])
+          (else (fn tree))
+          )))
