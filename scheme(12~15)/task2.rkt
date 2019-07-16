c3e66679ff5e6771cda588f0c463e4d4579eaf2b
arima0714 <a1710021@edu.cc.uec.ac.jp>
2019-07-16 12:58:01 +0900

    2-1できたっぽい

--- scheme(12~15)/task2.rkt
+++ scheme(12~15)/task2.rkt
@@ -2,15 +2,6 @@
   (read
    (open-input-file "./kakeizu")))
 
-"入力 begin"
-kakeizu
-"入力 end"
-
-"cdr test begin"
-(cdr kakeizu)
-"cdr test end"
-
-
 (define get-depth
   (lambda(tree num)
     (cond ((= num 1)
@@ -18,29 +9,65 @@ kakeizu
           );1になった時 -> この時は出力される
           ((null? tree)
           )
-          (else (map (lambda (t) (get-depth t (- num 1))) (cdr tree))
+          (else (apply append (map (lambda (t) (get-depth t (- num 1))) (cdr tree)))
           );1でない時 -> この時は息子をたどるようにする
     )
   )
 )
 
-(define myappend
-  (lambda (ls)
-           (apply append ls)
-           ))
-
-"get-depth kakeizu 1 begin"
+"get-depth 実行結果 begin"
+"get-depth kakeizu 1 <- executed"
 (get-depth kakeizu 1)
-"get-depth kakeizu 1 end"
-
-"get-depth kakeizu 3 begin"
+"get-depth kakeizu 3 <- executed"
 (get-depth kakeizu 3)
-"get-depth kakeizu 3 end"
-
-"get-depth kakeizu 6 begin"
+"get-depth kakeizu 6 <- executed"
 (get-depth kakeizu 6)
-"get-depth kakeizu 6 end"
+"get-depth 実行結果 end"
 
-"get-depth2 kakeizu 3 begin"
-(myappend (map append (get-depth kakeizu 3)))
-"get-depth2 kakeizu 3 end"
+(define equal_list
+  (lambda (tree name)
+    (cond
+         ((null? tree)
+          #f
+         )
+         ((equal? name (car tree))
+          #t
+         )
+         (else (equal_list (cdr tree) name)
+         )
+    )
+  )
+)
+
+
+(define search
+  (lambda (tree name depth)
+    (cond ((null? tree)
+           0
+          )
+          ((= depth 10)
+           0
+          )
+          ((equal_list (get-depth tree depth) name)
+           depth
+          )
+          (else (search tree name (+ depth 1))
+          )
+    )
+  )
+)
+
+(define get-cousin
+  (lambda (tree name)
+    (get-depth tree (search tree name 1))
+  )
+)
+
+"get-cousin 実行結果 begin"
+"(get-cousin kakeizu '秀忠 <- executed"
+(get-cousin kakeizu '秀忠)
+"(get-cousin kakeizu '吉宗 <- executed"
+(get-cousin kakeizu '吉宗)
+"(get-cousin kakeizu '家継 <- executed"
+(get-cousin kakeizu '家継)
+"get-cousin 実行結果 end"
