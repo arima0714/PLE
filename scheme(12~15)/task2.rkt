696ae1e0f682585eaff838c171b1a17f23dcbd7e
arima0714 <a1710021@edu.cc.uec.ac.jp>
2019-07-16 11:34:08 +0900

    7/16最初のコミット

--- scheme(12~15)/task2.rkt
+++ scheme(12~15)/task2.rkt
@@ -2,20 +2,27 @@
   (read
    (open-input-file "./kakeizu")))
 
-"入力"
+"入力 begin"
 kakeizu
-"入力"
+"入力 end"
 
 (define get-depth
   (lambda(tree num)
     (cond ((= num 1)
-              ()  
+              (map car (cdr tree))
           );1になった時 -> この時は出力される
+          ((null? tree)
+          )
           (else
-              ()
           );1でない時 -> この時は息子をたどるようにする
     )
   )
 )
 
-(get-depth kakeizu 3)
+"get-depth kakeizu 1 begin"
+(get-depth kakeizu 1)
+"get-depth kakeizu 1 end"
+
+"get-depth kakeizu 2 begin"
+(get-depth kakeizu 2)
+"get-depth kakeizu 2 end"
