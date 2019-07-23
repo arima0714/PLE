5acaf359dcd35556e7d4e528784b6c819d9d44bd
arima0714 <a1710021@edu.cc.uec.ac.jp>
2019-07-23 11:41:27 +0900

    課題3-1が終了

---
+++ scheme(12~15)/task3.rkt
@@ -0,0 +1,61 @@
+"Exercise3-1"
+
+(define diff
+  (lambda (func)
+    (cond
+         ((number? func)
+          0
+         )
+         ((equal? 'x func)
+          1
+         )
+         ((equal? '+ func)
+          '+
+         )
+         ((equal? '- func)
+          '-
+         )
+         ((equal? '* (car func))
+          `(+ (* ,(cadr func) ,(diff (caddr func))) (* ,(diff (cadr func)) ,(caddr func)))
+         )
+         ((equal? '** (car func))
+          `(* ,(caddr func) (* ,(diff (cadr func)) (** ,(cadr func) ,(- (caddr func) 1))))
+         )
+         (else
+          (map diff func)
+         )
+    )
+  )
+)
+
+"(cadr 式)で第1引数をとる"
+"(caddr 式)で第2引数をとる"
+
+#(diff 5)
+(diff 5)
+#(diff 'x)
+(diff 'x)
+#(diff '(+ x 5))
+(diff '(+ x 5))
+#(diff '(- x 5))
+(diff '(- x 5))
+#(diff '(* 5 x))
+(diff '(* 5 x))
+#(diff '(** x 2))
+(diff '(** x 2))
+#(diff '(+ (** x 3) (* 2 x) 4))
+(diff '(+ (** x 3) (* 2 x) 4))
+#(diff '(** (* x 3) 2))
+(diff '(** (* x 3) 2))
+#(diff '(* (+ x 2) (- (** x 2) x)))
+(diff '(* (+ x 2) (- (** x 2) x)))
+
+"Exercise3-2"
+
+(define ** expt)
+
+(define tangent
+  (lambda (func)
+    
+  )
+)
